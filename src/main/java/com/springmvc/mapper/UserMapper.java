c7ad510539aa77e38cf321729024449bf0660079
fengshow12345 <289963107@qq.com>
2018-08-24 10:32:10 +0800

    first commit

---
+++ src/main/java/com/springmvc/mapper/UserMapper.java
@@ -0,0 +1,35 @@
+package com.springmvc.mapper;
+
+import com.springmvc.pojo.User;
+import com.springmvc.pojo.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    int countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+
+    public List<User> queryAll() ;
+
+    List<User> queryUserByPage(@Param("username")String username,@Param("starttime") String starttime, @Param("endtime")String endtime);
+
+}
---
+++ src/main/java/com/springmvc/pojo/City.java
@@ -0,0 +1,32 @@
+package com.springmvc.pojo;
+public class City {
+
+
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public City(Integer id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+
+}
